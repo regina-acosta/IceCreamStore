10f9155e87784bda9d52578875020c40aa55d867
regina-acosta <maurelyac@gmail.com>
2025-05-12 13:59:23 -0400

    adding sql config, sql scripts to add prepopulated data for testing

---
+++ src/main/java/com/example/icecream/util/schema.sql
@@ -0,0 +1,60 @@
+CREATE TABLE customer (
+                          id UUID PRIMARY KEY,
+                          name VARCHAR(100) NOT NULL,
+                          email VARCHAR(100) NOT NULL UNIQUE,
+                          phone_number VARCHAR(15) UNIQUE,
+                          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE flavor (
+                          id UUID PRIMARY KEY,
+                          flavor_name VARCHAR(100) NOT NULL UNIQUE,
+                          description TEXT,
+                          status VARCHAR(20) NOT NULL CHECK (status IN ('active', 'inactive')),
+                          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE purchase (
+                          id UUID PRIMARY KEY,
+                          customer_id UUID NOT NULL,
+                          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                          total_price DECIMAL(7, 2) NOT NULL CHECK (total_price >= 0),
+                          CONSTRAINT fk_customer_purchase FOREIGN KEY (customer_id) REFERENCES customer(id)
+);
+
+CREATE TABLE purchase_item (
+                               id UUID PRIMARY KEY,
+                               purchase_id UUID NOT NULL,
+                               flavor_id UUID NOT NULL,
+                               quantity INT NOT NULL CHECK (quantity > 0),
+                               unit_price DECIMAL(5, 2) NOT NULL,
+
+                               CONSTRAINT fk_purchase FOREIGN KEY (purchase_id) REFERENCES purchase(id),
+                               CONSTRAINT fk_flavor_purchase_item FOREIGN KEY (flavor_id) REFERENCES flavor(id)
+);
+
+Create TABLE monthly_menu_item (
+                          id UUID PRIMARY KEY,
+                          menu_month INT NOT NULL CHECK (menu_month BETWEEN 1 AND 12),
+                          menu_year INT NOT NULL,
+                          flavor_id UUID NOT NULL,
+                          unit_price DECIMAL(5, 2) NOT NULL,
+                          rank_score INT NOT NULL,
+                          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                          CONSTRAINT fk_flavor_monthly_menu FOREIGN KEY (flavor_id) REFERENCES flavor(id),
+                        -- Prevent duplicates for same flavor in the same month
+                          CONSTRAINT unique_flavor_per_month UNIQUE (flavor_id, menu_month, menu_year)
+);
+
+CREATE TABLE vote (
+                          id UUID PRIMARY KEY,
+                          customer_id UUID NOT NULL,
+                          flavor_id UUID NOT NULL,
+                          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                          vote_month INT NOT NULL CHECK (vote_month BETWEEN 1 AND 12),
+                          vote_year INT NOT NULL,
+                          CONSTRAINT fk_customer_vote FOREIGN KEY (customer_id) REFERENCES customer(id),
+                          CONSTRAINT fk_flavor_vote FOREIGN KEY (flavor_id) REFERENCES flavor(id),
+                          -- Prevent multiple votes for the same flavor by the same customer in the same month and year
+                          CONSTRAINT unique_vote UNIQUE (customer_id, flavor_id, vote_month, vote_year)
+);
